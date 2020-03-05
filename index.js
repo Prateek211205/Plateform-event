1726eafe0481b02b57a38b595ba900b94853f5ee
Prateek Chaturvedi <prateekchaturvedi@Prateeks-Air.Dlink>
2020-03-05 22:31:47 +0530

    new commit

--- index.js
+++ index.js
@@ -4,8 +4,13 @@ var mustacheExpress = require('mustache-express');
 const session = require('express-session');
 const bodyParser = require('body-parser');
 const requestSalesforce = require('request');
+var MemoryStore = require('memorystore')(session)
 //initialize session
-app.use(session({secret: 'S3CRE7', resave: true, saveUninitialized: true}));
+app.use(session({secret: 'S3CRE7', 
+store: new MemoryStore({
+  checkPeriod: 86400000 // prune expired entries every 24h
+}),
+resave: true, saveUninitialized: true}));
 
 app.engine('html', mustacheExpress());
 app.set('view engine', 'html');
