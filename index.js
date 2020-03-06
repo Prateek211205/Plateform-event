4a0b2bc0fed79ab8be245745cedaa4cf059c8eff
Prateek Chaturvedi <prateekchaturvedi@Prateeks-Air.Dlink>
2020-03-06 22:10:17 +0530

    newcommit

--- index.js
+++ index.js
@@ -32,17 +32,65 @@ app.use(express.static(__dirname + '/public'));
 
 app.set('views', __dirname+'/views');
 
+let salesforce;
 app.get('/', (req, res) => {
-     console.log('------hello---');
-    /*var conn = new jsforce.Connection({oauth2:data});
+    var conn = new jsforce.Connection({oauth2:data});
     conn.login('prateek211205@appirio.com','sanu2112061Ilj0KngZrRKFjtZWG7jkGDNL', function(err, userInfo) {
         console.log(err);
         req.session.accessToken = conn.accessToken;
         req.session.instanceUrl = conn.instanceUrl;
         res.render('index');
-    });*/
-    res.render('index');
+    });
     
+});
+
+
+
+app.get('/case', (request, response) => {
+
+    let caseId = request.query.id;
+    if(!caseId){
+        return;
+    }
+    console.log(caseId);
+    let conn = new jsforce.Connection({
+        oauth2 : data,
+        accessToken: request.session.accessToken,
+        instanceUrl: request.session.instanceUrl
+    });
+    var data = [{label:'New', active: false},{label:'Working', active: false},{label:'Escalated', active: false}];
+    conn.sobject("Case").retrieve(caseId, function(err, result) {
+       if(!result){return}
+       for(let item in data){
+            data[item].active = true
+            if(data[item].label == result.Status){
+              break;
+            }
+        }
+        response.send({result,data});
+    });
+});
+
+app.post('/case', (request, response) => {
+  let url =  request.session.instanceUrl+'/services/data/v48.0/sobjects/Case_Demo__e/';
+  const options = {
+    url,
+    json: true,
+    'auth': {
+      'bearer': request.session.accessToken
+    },
+    body: {
+       Case_Id__c: request.body.caseId,
+       Performed_By__c: 'Prateek  Chaturvedi',
+       Status__c : request.body.status
+    }
+};
+requestSalesforce.post(options, function(err, httpResponse, body){
+    response.send(httpResponse.body.success);
+   
+});
+  
+  
 });
 
 var PORT = process.env.PORT || 5000;
